acdaa2a879da9b173dd2e7b228669693246ca53d
FerchichiXIII <91852763+FerchichiXIII@users.noreply.github.com>
2022-12-08 22:31:38 +0000

    lockboces

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,7 +12,7 @@ def canUnlockAll(boxes):
         if len(i) == 0 and i is not boxes[k-1]:
             return False
 
-    for index, keys in enumerate(boxes):
+    for index, k in enumerate(boxes):
         if index in newlist or index < k-1:
             return True
         else:
