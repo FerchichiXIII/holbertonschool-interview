5edf32d0d32205d14525f208b7cb5768f2c61e0a
FerchichiXIII <anasferchichi22@gmail.com>
2022-12-12 12:48:58 +0100

    push all

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: listint
+ * @number: int
+ * Return: NULL if failed else Return thr add of the new node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    
+}
