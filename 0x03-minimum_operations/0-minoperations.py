444d58e766a2587bcaf7cda71f93696c3e17dda2
FerchichiXIII <91852763+FerchichiXIII@users.noreply.github.com>
2023-02-28 17:16:29 +0000

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,14 +4,17 @@
 """
 calculates the fewest number of operations .
 """
-import math
 
 
 def minOperations(n):
-
-    if n == 1:
+    result = 0
+    index = 2
+    if n < 2:
         return 0
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return i + minOperations(n // i)
-    return n
+    while (index < n + 1):
+        while n % index == 0:
+            result += index
+
+            n /= index
+        index += 1
+    return result
