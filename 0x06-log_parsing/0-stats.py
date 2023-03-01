781303553f53e8c263cab2f097962d3d36ab87c1
FerchichiXIII <91852763+FerchichiXIII@users.noreply.github.com>
2023-03-01 11:49:20 +0000

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 
-
 import sys
-from collections import defaultdict
 
-# Define a dictionary to keep track of the counts of each status code
-status_counts = defaultdict(int)
-
-# Initialize total file size to 0
-total_file_size = 0
-
-# Read from stdin line by line
-for i, line in enumerate(sys.stdin):
-    try:
-        # Parse the line according to the input format
-        ip_address, date, request, status_code, file_size = line.split(' ')
-        request = request.strip('"')
-        if request != 'GET /projects/260 HTTP/1.1':
-            continue
-        status_code = int(status_code)
-        file_size = int(file_size)
-
-        # Update the total file size
-        total_file_size += file_size
-
-        # Update the count of the status code
-        status_counts[status_code] += 1
-
-        # Print statistics every 10 lines
-        if i > 0 and i % 10 == 0:
-            print(f'Total file size: {total_file_size}')
-            for status_code in sorted(status_counts.keys()):
-                print(f'{status_code}: {status_counts[status_code]}')
-
-    except KeyboardInterrupt:
-        # If CTRL + C is pressed, print the final statistics and exit
-        print(f'Total file size: {total_file_size}')
-        for status_code in sorted(status_counts.keys()):
-            print(f'{status_code}: {status_counts[status_code]}')
-        sys.exit()
-    except:
-        # If the line doesn't match the input format, skip it
-        continue
+i = 0
+FileSize = 0
+STATUS = {'200': 0, '301': 0,
+          '400': 0, '401': 0,
+          '403': 0, '404': 0,
+          '405': 0, '500': 0}
+try:
+    for line in sys.stdin:
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            FileSize += int(sp[-1])
+            if sp[-2] in STATUS:
+                STATUS[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for k, v in sorted(STATUS.items()):
+                if v != 0:
+                    print("{}: {}".format(k, v))
+finally:
+    print("File size: {}".format(FileSize))
+    for k, v in sorted(STATUS.items()):
+        if v != 0:
+            print("{}: {:d}".format(k, v))
