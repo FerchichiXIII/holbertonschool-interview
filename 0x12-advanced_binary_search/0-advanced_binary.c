efd618b4de38e6d6311b5f7cf8dc9cc88e5256a6
FerchichiXIII <91852763+FerchichiXIII@users.noreply.github.com>
2023-05-06 13:06:56 +0000

    Advanced Binary Search

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,36 +1,57 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * advanced_binary - Searches for a value in a sorted array of integers
- * @array: Pointer to the first element of the array
- * @size: Number of elements in array
+ * print_array - Print the array being searched
+ * @array: Array to print
+ * @left: Left index of the @array to print
+ * @right: Right index of the @array to print
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = left; i <= right; i++)
+    {
+        printf("%d", array[i]);
+        if (i != right)
+            printf(", ");
+    }
+    printf("\n");
+}
+
+/**
+ * advanced_binary - Search for a value in a sorted array of integers
+ * @array: Array to search in
+ * @size: Number of elements in the @array
  * @value: Value to search for
- * Return: Index where value is located or -1 if not found
+ *
+ * Return: Index where @value is located, or -1 if @value is not present in @array
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t i = 0;
-    int index;
+    size_t left = 0, right = size - 1, mid;
 
-    if (!array || size == 0)
+    if (!array)
         return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-        printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
-
-    if (size == 1 && array[0] != value)
-        return (-1);
-
-    index = (size - 1) / 2;
-
-    if (array[index] == value)
+    while (left <= right)
     {
-        if (index == 0 || array[index - 1] != value)
-            return (index);
-        return (advanced_binary(array, index + 1, value));
+        mid = (left + right) / 2;
+        print_array(array, left, right);
+
+        if (array[mid] < value)
+            left = mid + 1;
+        else if (array[mid] > value)
+            right = mid - 1;
+        else
+        {
+            if (mid == 0 || array[mid - 1] != value)
+                return (mid);
+            right = mid;
+        }
     }
-    if (array[index] < value)
-        return (advanced_binary(&array[index + 1], size - index - 1, value));
-    return (advanced_binary(array, index + 1, value));
+
+    return (-1);
 }
