f462a7ae0c52002a983e0bd555577b2eec6cc6e5
root <root@FerchichiXIII.localdomain>
2023-06-01 00:21:42 +0100

    fix pycode

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,7 +3,6 @@
 def rotate_2d_matrix(matrix):
     n = len(matrix)
 
-
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
