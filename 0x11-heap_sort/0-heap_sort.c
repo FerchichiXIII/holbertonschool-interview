a7efb8a44bf18b7a3c371324d9da541b4aa2a457
root <root@FerchichiXIII.localdomain>
2023-06-05 07:47:37 +0100

    fix betty

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -7,27 +7,32 @@
  * @root: Index of the root node
  * @heap_size: Size of the heap
  */
- 
- void heapify(int *array, size_t size, size_t root, size_t heap_size)
+
+void heapify(int *array, size_t size, size_t root, size_t heap_size)
 {
-    size_t largest = root;
-    size_t left = 2 * root + 1;
-    size_t right = 2 * root + 2;
+	size_t largest = root;
+	size_t left = 2 * root + 1;
+	size_t right = 2 * root + 2;
 
-    if (left < heap_size && array[left] > array[largest])
-        largest = left;
+	if (left < heap_size && array[left] > array[largest])
+		largest = left;
 
-    if (right < heap_size && array[right] > array[largest])
-        largest = right;
+	if (right < heap_size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != root)
-    {
-        int temp = array[root];
-        array[root] = array[largest];
-        array[largest] = temp;
-        print_array(array, size);
-        heapify(array, size, largest, heap_size);
-    }
+	if (largest != root)
+	{
+
+		int temp = array[root];
+
+		array[root] = array[largest];
+
+		array[largest] = temp;
+
+		print_array(array, size);
+
+		heapify(array, size, largest, heap_size);
+	}
 }
 
 /**
@@ -38,17 +43,19 @@
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
+	int i;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, i, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+	for (i = size - 1; i > 0; i--)
+	{
+		int temp = array[0];
 
-    for (i = size - 1; i > 0; i--)
-    {
-        int temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
-        print_array(array, size);
-        heapify(array, size, 0, i);
-    }
+		array[0] = array[i];
+
+		array[i] = temp;
+
+		print_array(array, size);
+		heapify(array, size, 0, i);
+	}
 }
