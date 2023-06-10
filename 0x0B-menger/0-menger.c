4bc9b3c814401bfff2626108dc97a5eb1c7c0949
root <root@FerchichiXIII.localdomain>
2023-06-10 20:14:53 +0100

    fix betty

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,33 +6,33 @@
  */
 void menger(int level)
 {
-    int size = (int)pow(3, level);
-    int row, col;
+	int size = (int)pow(3, level);
+	int row, col;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            int x = col;
-            int y = row;
-            char symbol = '#';
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			int x = col;
+			int y = row;
+			char symbol = '#';
 
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    symbol = ' ';
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					symbol = ' ';
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
 
-            printf("%c", symbol);
-        }
-        printf("\n");
-    }
+			printf("%c", symbol);
+		}
+		printf("\n");
+	}
 }
