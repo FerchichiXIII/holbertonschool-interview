d62cabb25c58baa7f999fba106292bfb808eddf4
root <root@FerchichiXIII.localdomain>
2023-06-10 23:24:11 +0100

    Star Wars Characters

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Status:', response.statusCode);
+    } else {
+        const filmData = JSON.parse(body);
+        const characterUrls = filmData.characters;
+
+        characterUrls.forEach((characterUrl) => {
+            request(characterUrl, (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else if (response.statusCode !== 200) {
+                    console.error('Status:', response.statusCode);
+                } else {
+                    const characterData = JSON.parse(body);
+                    console.log(characterData.name);
+                }
+            });
+        });
+    }
+}); 
