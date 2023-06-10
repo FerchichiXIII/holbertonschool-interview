299cc8c10cd137cc134c3321462fdfed817bccdb
root <root@FerchichiXIII.localdomain>
2023-06-10 22:57:36 +0100

    AVL - From sorted array

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -2,53 +2,37 @@
 #include "binary_trees.h"
 
 /**
- * create_avl_tree - Creates an AVL tree from a sorted array
- *
- * @array: Pointer to the first element of the sorted array
- * @start: Index of the start element
- * @end: Index of the end element
- *
- * Return: Pointer to the root node of the created AVL tree
- */
-avl_t *create_avl_tree(int *array, int start, int end)
-{
-    avl_t *new_node;
-    int mid;
-
-    if (start > end)
-        return NULL;
-
-    mid = (start + end) / 2;
-
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->n = array[mid];
-    new_node->parent = NULL;
-    new_node->left = create_avl_tree(array, start, mid - 1);
-    new_node->right = create_avl_tree(array, mid + 1, end);
-
-    if (new_node->left != NULL)
-        new_node->left->parent = new_node;
-    if (new_node->right != NULL)
-        new_node->right->parent = new_node;
-
-    return new_node;
-}
-
-/**
  * sorted_array_to_avl - Builds an AVL tree from a sorted array
- *
- * @array: Pointer to the first element of the sorted array
+ * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  *
- * Return: Pointer to the root node of the created AVL tree
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (array == NULL || size == 0)
-        return NULL;
+    avl_t *new_node;
+    size_t mid;
 
-    return create_avl_tree(array, 0, size - 1);
+    if (size == 0)
+        return (NULL);
+
+    mid = size / 2;
+
+    /* Create new node for the middle element */
+    new_node = malloc(sizeof(avl_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = array[mid];
+    new_node->parent = NULL;
+    new_node->left = sorted_array_to_avl(array, mid);
+    new_node->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
+
+    if (new_node->left != NULL)
+        new_node->left->parent = new_node;
+
+    if (new_node->right != NULL)
+        new_node->right->parent = new_node;
+
+    return (new_node);
 }
