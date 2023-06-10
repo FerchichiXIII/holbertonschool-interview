385d065184928fe6c598b3b60bdd4e8428408099
root <root@FerchichiXIII.localdomain>
2023-06-10 22:59:56 +0100

    fix betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,29 +10,29 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *new_node;
-    size_t mid;
+	avl_t *new_node;
+	size_t mid;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    mid = size / 2;
+	mid = size / 2;
 
-    /* Create new node for the middle element */
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Create new node for the middle element */
+	new_node = malloc(sizeof(avl_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = array[mid];
-    new_node->parent = NULL;
-    new_node->left = sorted_array_to_avl(array, mid);
-    new_node->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
+	new_node->n = array[mid];
+	new_node->parent = NULL;
+	new_node->left = sorted_array_to_avl(array, mid);
+	new_node->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
 
-    if (new_node->left != NULL)
-        new_node->left->parent = new_node;
+	if (new_node->left != NULL)
+		new_node->left->parent = new_node;
 
-    if (new_node->right != NULL)
-        new_node->right->parent = new_node;
+	if (new_node->right != NULL)
+		new_node->right->parent = new_node;
 
-    return (new_node);
+	return (new_node);
 }
