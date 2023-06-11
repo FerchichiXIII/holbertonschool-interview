ac556da34f23e4a9c84fdb79f3cff1a53365d624
root <root@FerchichiXIII.localdomain>
2023-06-11 11:51:44 +0100

    add heder file

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h> 
+
+int _putchar(char c);
+int _strlen(char *s);
+void _print_number(char *number);
+void multiply(char *num1, char *num2);
+
+#endif /* HOLBERTON_H */
