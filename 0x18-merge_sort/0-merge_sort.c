466147929caa0af16d07bf58e96985aa50ac66f6
root <root@FerchichiXIII.localdomain>
2023-06-22 12:06:05 +0100

    fix all

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -3,74 +3,71 @@
 #include <stdio.h>
 
 /**
- * merge - Merges two subarrays of `array`
- * @array: The array to be sorted
+ * merge - Merges two subarrays of 'array'
+ * @array: The array containing the subarrays
  * @left: Pointer to the left subarray
  * @left_size: Size of the left subarray
  * @right: Pointer to the right subarray
  * @right_size: Size of the right subarray
  **/
 void merge(int *array, int *left, size_t left_size,
-							 int *right, size_t right_size)
+		   int *right, size_t right_size)
 {
 	size_t i = 0, j = 0, k = 0;
-	int *tmp = malloc((left_size + right_size) * sizeof(int));
+	size_t total_size = left_size + right_size;
+	int *tmp = malloc(sizeof(int) * total_size);
 
-	printf("Merging...\n[left]: ");
+	if (tmp == NULL)
+		return;
+
+	printf("Merging...\n");
+	printf("[left]: ");
 	print_array(left, left_size);
 	printf("[right]: ");
 	print_array(right, right_size);
+
 	while (i < left_size && j < right_size)
 	{
 		if (left[i] <= right[j])
-		{
-			tmp[k] = left[i];
-			i++;
-		}
+			tmp[k++] = left[i++];
 		else
-		{
-			tmp[k] = right[j];
-			j++;
-		}
-		k++;
-	}
-	while (i < left_size)
-	{
-		tmp[k] = left[i];
-		i++;
-		k++;
-	}
-	while (j < right_size)
-	{
-		tmp[k] = right[j];
-		j++;
-		k++;
+			tmp[k++] = right[j++];
 	}
 
-	for (i = 0; i < left_size + right_size; i++)
+	while (i < left_size)
+		tmp[k++] = left[i++];
+
+	while (j < right_size)
+		tmp[k++] = right[j++];
+
+	for (i = 0; i < total_size; i++)
 		array[i] = tmp[i];
 
 	printf("[Done]: ");
-	print_array(array, left_size + right_size);
+	print_array(array, total_size);
+
 	free(tmp);
 }
+
 /**
  * merge_sort - Sorts an array of integers in ascending order using
  *              the Merge Sort algorithm
  * @array: The array to be sorted
- * @size: Number of elements in the array
+ * @size: Number of elements in @array
  **/
 void merge_sort(int *array, size_t size)
 {
 	size_t mid;
 	int *left, *right;
 
-	if (array == NULL || size < 2)
+	if (size < 2 || array == NULL)
 		return;
 
 	mid = size / 2;
+
 	left = array;
 	right = array + mid;
+
 	merge_sort(left, mid);
 	merge_sort(right, size - mid);
 	merge(array, left, mid, right, size - mid);
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -2,8 +2,6 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
