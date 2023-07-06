ced2906dd2752efa3c0e72b3b2025cb7e0759277
root <root@FerchichiXIII.localdomain>
2023-07-06 22:58:47 +0100

    fix (:

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-
 """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total.
+list calculation
 """
 
 
 def makeChange(coins, total):
-    """Make Chanhe"""
+    """
+    make change: bottom up method
+    """
     if total <= 0:
         return 0
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for c in coins:
-        for i in range(c, total + 1):
-            dp[i] = min(dp[i], dp[i - c] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
     return dp[total] if dp[total] != float('inf') else -1
