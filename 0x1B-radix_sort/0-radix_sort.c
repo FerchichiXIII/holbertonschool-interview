d152efdfafb01b23b65e9767bbc24f65974ba0c5
root <root@FerchichiXIII.localdomain>
2023-08-20 20:33:40 +0100

    Radix sort

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -9,8 +9,9 @@
 int get_max(int *array, size_t size)
 {
 	int max = array[0];
+	size_t i;
 
-	for (size_t i = 1; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
@@ -27,25 +28,25 @@ int get_max(int *array, size_t size)
 void counting_sort(int *array, size_t size, int exp)
 {
 	int *output = malloc(sizeof(int) * size);
+	int count[10] = {0};
+	size_t i;
 
 	if (output == NULL)
 		return;
 
-	int count[10] = {0};
-
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		count[(array[i] / exp) % 10]++;
 
-	for (int i = 1; i < 10; i++)
+	for (i = 1; i < 10; i++)
 		count[i] += count[i - 1];
 
-	for (int i = size - 1; i >= 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
 		output[count[(array[i] / exp) % 10] - 1] = array[i];
 		count[(array[i] / exp) % 10]--;
 	}
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = output[i];
 
 	free(output);
@@ -62,8 +63,9 @@ void radix_sort(int *array, size_t size)
 		return;
 
 	int max = get_max(array, size);
+	int exp;
 
-	for (int exp = 1; max / exp > 0; exp *= 10)
+	for (exp = 1; max / exp > 0; exp *= 10)
 	{
 		counting_sort(array, size, exp);
 		print_array(array, size);
