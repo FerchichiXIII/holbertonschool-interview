aa5bad89186cd4720be98dea6e26700f512362a2
root <root@FerchichiXIII.localdomain>
2023-10-23 09:58:23 +0100

    fix it

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,20 +4,20 @@ const request = require('request');
 const Url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(Url, async (error, response, body) => {
-    if (error) {
-        console.log(error);
-    }
-    response = JSON.parse(body);
-    for (const charId of response.characters) {
-        await new Promise((resolve, reject) => {
-            request(charId, (error, response, body) => {
-                if (error) {
-                    console.log(error);
-                }
-                const formatedResponse = JSON.parse(body);
-                console.log(formatedResponse.name);
-                resolve();
-            });
-        });
-    }
+	if (error) {
+		console.log(error);
+	}
+	response = JSON.parse(body);
+	for (const charId of response.characters) {
+		await new Promise((resolve, reject) => {
+			request(charId, (error, response, body) => {
+				if (error) {
+					console.log(error);
+				}
+				const formatedResponse = JSON.parse(body);
+				console.log(formatedResponse.name);
+				resolve();
+			});
+		});
+	}
 });
