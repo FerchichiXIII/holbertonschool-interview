e71bcb6c2863d80aa4726cb99672eefaf7ad519b
root <root@FerchichiXIII.localdomain>
2023-10-23 14:57:00 +0100

    nqueens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,55 +1,50 @@
 #!/usr/bin/python3
-
-
+"""N queens classical problem."""
 import sys
 
-
-def get_sol(board, n):
-    """Get solution from board."""
-    sols = []
+def get_solutions(board, n):
+    """Get solutions from board."""
+    solutions = []
     for x in range(n):
         for y in range(n):
             if board[x][y]:
-                sols.append([x, y])
-    return sols
+                solutions.append([x, y])
+    return solutions
 
-
-def isvalid(board, pos, n):
+def is_valid_position(board, position, n):
     """Check if position is valid on the board."""
     for i in range(0, n):
-        if board[pos[0]][i]:
+        if board[position[0]][i]:
             return False
-        if board[i][pos[1]]:
+        if board[i][position[1]]:
             return False
-        if pos[0] + i < n and pos[1] + i < n:
-            if board[pos[0] + i][pos[1] + i]:
+        if position[0] + i < n and position[1] + i < n:
+            if board[position[0] + i][position[1] + i]:
                 return False
-        if pos[0] - i >= 0 and pos[1] - i >= 0:
-            if board[pos[0] - i][pos[1] - i]:
+        if position[0] - i >= 0 and position[1] - i >= 0:
+            if board[position[0] - i][position[1] - i]:
                 return False
-        if pos[0] + i < n and pos[1] - i >= 0:
-            if board[pos[0] + i][pos[1] - i]:
+        if position[0] + i < n and position[1] - i >= 0:
+            if board[position[0] + i][position[1] - i]:
                 return False
-        if pos[0] - i >= 0 and pos[1] + i < n:
-            if board[pos[0] - i][pos[1] + i]:
+        if position[0] - i >= 0 and position[1] + i < n:
+            if board[position[0] - i][position[1] + i]:
                 return False
     return True
 
-
-def solvenqueen(board, col, n):
-    """Solve n queen using backtracking."""
+def solve_n_queen(board, col, n):
+    """Solve N queens using backtracking."""
     if col >= n:
-        print(get_sol(board, n))
+        print(get_solutions(board, n))
         return
     for i in range(n):
-        if isvalid(board, (i, col), n):
+        if is_valid_position(board, (i, col), n):
             board[i][col] = True
-            solvenqueen(board, col + 1, n)
+            solve_n_queen(board, col + 1, n)
             board[i][col] = False
 
-
 def main():
-    """Solve n queen using backtracking."""
+    """Solve N queens using backtracking."""
     if len(sys.argv) == 1:
         print("Usage: nqueens N")
         exit(1)
@@ -60,11 +55,11 @@ def main():
         exit(1)
 
     if N < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
     board = [[False for _ in range(N)] for _ in range(N)]
 
-    solvenqueen(board, 0, N)
+    solve_n_queen(board, 0, N)
 
-
-main()
+if __name__ == "__main__":
+    main()
